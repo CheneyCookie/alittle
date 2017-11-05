8c1bcd45158e1dba32a079f3919e611bf1c511f0
cheney <cheney@192.168.16.1>
2017-11-05 10:08:25 +0800

    Spring MVC

---
+++ SpringMVC-2/src/com/cheney/springmvc/test/SpringMVCTest.java
@@ -0,0 +1,148 @@
+package com.cheney.springmvc.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cheney.springmvc.crud.dao.EmployeeDao;
+import com.cheney.springmvc.crud.entities.Employee;
+
+
+@Controller
+public class SpringMVCTest {
+	
+	@Autowired
+	private EmployeeDao employeeDao;
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
+	
+	@RequestMapping("/testSimpleMappingExceptionResolver")
+	public String testSimpleMappingExceptionResolver(@RequestParam("i") int i){
+		String[] vals=new String[10];
+		System.out.println(vals[i]);
+		return "success";
+	}
+	
+	@ResponseStatus(reason="测试",value=HttpStatus.NOT_FOUND)
+	@RequestMapping("/testResourceStatusExceptionResolver")
+	public String testResourceStatusExceptionResolver(@RequestParam("i") int i){
+		if(i==13){
+			throw new UserNameNotMatchPasswordException();
+		}
+		System.out.println("testResourceStatusExceptionResolver... ");
+		return "success";
+	}
+	
+	/*@ExceptionHandler({RuntimeException.class})
+	public ModelAndView hanldeArithmeticException2(Exception ex){
+		
+		System.out.println("[出异常了]"+ ex);
+		ModelAndView mv=new ModelAndView("error");
+		mv.addObject("exception",ex);
+		return mv;
+	}*/
+	
+	/**
+	 * 1.在 @ExceptionHandler 方法的入参中可以加入Exception类型的参数，该参数即对应发生的异常对象
+	 * 2. @ExceptionHandler 方法的入参中不能传入Map，若希望把异常信息传到页面上，需要使用是 ModelAndView作为返回值
+	 * 3. @ExceptionHandler 标记的异常有优先级的问题
+	 * 4. @ControllerAdvice 如果在当前Handler中找不到 @ExceptionHandler 方法来处理当前方法出现的异常，
+	 * 		则将去 @ControllerAdvice 标记的类中查找 @ExceptionHandler 标记的方法来处理异常
+	 * 
+	 * @param ex
+	 * @param map
+	 * @return
+	 */
+	/*@ExceptionHandler({ArithmeticException.class})
+	public ModelAndView hanldeArithmeticException(Exception ex){
+		
+		System.out.println("出异常了"+ ex);
+		ModelAndView mv=new ModelAndView("error");
+		mv.addObject("exception",ex);
+		return mv;
+	}*/
+	
+	@RequestMapping("/testExceptionHandlerExceptionResolver")
+	public String testExceptionHandlerExceptionResolver(@RequestParam("i") int i){
+		System.out.println("result: "+(10/i));
+		return "success";
+	}
+	
+	@RequestMapping("/testFileUpload")
+	public String testFileUpload(@RequestParam("desc") String desc,@RequestParam("file") MultipartFile file) throws IOException{
+		System.out.println("desc: "+desc);
+		System.out.println("OriginalFilename: "+file.getOriginalFilename());
+		System.out.println("InputStream: "+file.getInputStream());
+		return "success";
+	}
+	
+	@RequestMapping("/i18n")
+	public String testI18n(Locale locale){
+		String val=messageSource.getMessage("i18n.user",null, locale);
+		System.out.println(val);
+		return "i18n";
+	}
+	
+	@RequestMapping("/testResponseEntity")
+	public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException{
+		
+		byte[] body=null;
+		ServletContext servletContext=session.getServletContext();
+		InputStream in=servletContext.getResourceAsStream("/files/note.txt");
+		body=new byte[in.available()];
+		in.read(body);
+		
+		HttpHeaders headers=new HttpHeaders();
+		headers.add("Content-Disposition", "attachment;filename=note.txt");
+		
+		HttpStatus status=HttpStatus.OK;
+		
+		
+		ResponseEntity<byte[]> response=new ResponseEntity<byte[]>(body, headers, status);
+		return response;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/testHttpMessageConvertor")
+	public String testHttpMessageConvertor(@RequestBody String body){
+		System.out.println(body);
+		return "helloworld! "+new Date();
+	}
+	
+	@ResponseBody
+	@RequestMapping("/testJson")
+	public Collection<Employee> testJson(){
+		return employeeDao.getAll();
+	}
+	
+	@RequestMapping("/testConversionServiceConverer")
+	public String testConverter(@RequestParam("employee") Employee employee){
+		System.out.println("save:"+employee);
+		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
+	
+}
